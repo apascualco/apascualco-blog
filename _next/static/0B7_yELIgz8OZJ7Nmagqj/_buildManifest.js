ef9b084b942c68d3bd4050ab9aa38b3ac5b778ae
apascualco <apascualco@gmail.com>
2022-05-09 20:36:44 +0000

    Deploying to gh-pages from  @ d968ffbb912601d18993ef40753b413aecfcc2a5 🚀

---
+++ _next/static/0B7_yELIgz8OZJ7Nmagqj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/129-bdab51afe0d3370e.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-b370678e5a358549.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
